9ceed5109043dcac91d7129abffd26c4cca05d2d
Nithya <nithya.sivaprakasam@syncfusion.com>
2025-05-20 18:02:03 +0530

    Sample Added

---
+++ CustomAdaptor-as-component/wwwroot/JavaScript.js
@@ -0,0 +1,11 @@
+﻿window.selectContent = function (id) {
+    document.getElementsByClassName("e-flm_optrdiv")[0].style.display = "none";
+    var btns = document.querySelector('.e-dialog').querySelectorAll('.e-btn');
+    for (var i = 0; i < btns.length; i++)
+    {
+
+        btns[i].style.display = "none";
+
+    }
+
+}
