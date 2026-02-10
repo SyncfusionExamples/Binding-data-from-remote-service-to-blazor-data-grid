c070b1578b32a2369bacd2748a3ac62ff139b7d5
SadhanaBaskaran <sadhana.baskaran@syncfusion.com>
2026-02-10 17:03:58 +0530

    1008467: Updated the WebApiAdaptor documentation samples in DataGrid

--- WebApiAdaptor/WebApiAdaptor/WebApiAdaptor/Program.cs
+++ WebApiAdaptor/WebApiAdaptor/WebApiAdaptor/Program.cs
@@ -1,32 +1,35 @@
-using Syncfusion.Blazor;
+using WebApiAdaptor.Client.Pages;
 using WebApiAdaptor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddControllers();
-
-builder.Services.AddSyncfusionBlazor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-app.MapControllers();
 
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(WebApiAdaptor.Client._Imports).Assembly);
 
 app.Run();
