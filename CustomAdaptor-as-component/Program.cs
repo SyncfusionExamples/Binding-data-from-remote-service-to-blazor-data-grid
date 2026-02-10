125d19f21b4171391f55f0cb577c6f8cafc4e8f3
anandx4604 <anand.aruldoss@syncfusion.com>
2026-02-10 11:08:36 +0530

    1008186: Revamped the Old CustomAdaptor as component Project as Net10 Project

--- CustomAdaptor-as-component/Program.cs
+++ CustomAdaptor-as-component/Program.cs
@@ -1,17 +1,11 @@
-using BlazorApp1.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Localization;
+using CustomAdaptorAsComponent.Components;
 using Syncfusion.Blazor;
-using System.Globalization;
-using static BlazorApp1.Pages.Index;
-
+using static CustomAdaptorAsComponent.Components.Pages.Home;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped<Order>();
 
@@ -20,18 +14,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
